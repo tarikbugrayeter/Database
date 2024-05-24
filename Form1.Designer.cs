1a39ccb254753126e4756acf73614b118f9c0fcb
Tarık Buğra <Tark Bura@DESKTOP-QMQAOKM>
2024-05-24 16:40:17 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Database
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(119, 402);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(99, 45);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "Start";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 195);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(119, 345);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(119, 291);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 26);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "B User Count";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "A User Count";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(644, 182);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(274, 26);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(644, 270);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(274, 26);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(640, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(218, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Deadlock Counter For A User";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(640, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(218, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Deadlock Counter For B User";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1529, 735);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }   
+}
+
---
+++ Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Database
+{
+    public partial class Form1 : Form
+    {
+        private readonly string connectionString = "Server=.\\SQLEXPRESS;Database=AdventureWorks2022;Integrated Security=True;";
+        private int deadlockCountA = 0;
+        private int deadlockCountB = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // ComboBox'a öğeler ekleyin
+            comboBox1.Items.Add("READ UNCOMMITTED");
+            comboBox1.Items.Add("READ COMMITTED");
+            comboBox1.Items.Add("REPEATABLE READ");
+            comboBox1.Items.Add("SERIALIZABLE");
+
+            // Default olarak ilk öğeyi seçin (isteğe bağlı)
+            comboBox1.SelectedIndex = 0;
+
+            // Deadlock etiketlerinin başlangıç değeri
+            textBox3.Text = "Type A Users Deadlocks: 0";
+            textBox4.Text = "Type B Users Deadlocks: 0";
+        }
+
+        private void TypeAUserTransaction(string isolationLevel)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Transaction isolation level belirlemek için
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = $"SET TRANSACTION ISOLATION LEVEL {isolationLevel}";
+                    command.ExecuteNonQuery();
+                }
+
+                // Başlangıç ve bitiş tarihleri
+                DateTime beginDate = new DateTime(2011, 01, 01);
+                DateTime endDate = new DateTime(2015, 12, 31);
+
+                for (int i = 0; i < 100; i++)
+                {
+                    try
+                    {
+                        using (SqlTransaction transaction = connection.BeginTransaction())
+                        {
+                            // Rastgele tarih seçimi
+                            DateTime transactionDate = GetRandomDate(beginDate, endDate);
+
+                            // Update sorgusu için parametre değerleri
+                            SqlParameter paramBeginDate = new SqlParameter("@BeginDate", transactionDate);
+                            SqlParameter paramEndDate = new SqlParameter("@EndDate", transactionDate.AddYears(1).AddDays(-1));
+
+                            // Update sorgusu
+                            string updateQuery = "UPDATE Sales.SalesOrderDetail " +
+                                                 "SET UnitPrice = UnitPrice * 10.0 / 10.0 " +
+                                                 "WHERE UnitPrice > 100 " +
+                                                 "AND EXISTS (SELECT * FROM Sales.SalesOrderHeader " +
+                                                             "WHERE Sales.SalesOrderHeader.SalesOrderID = Sales.SalesOrderDetail.SalesOrderID " +
+                                                             "AND Sales.SalesOrderHeader.OrderDate BETWEEN @BeginDate AND @EndDate " +
+                                                             "AND Sales.SalesOrderHeader.OnlineOrderFlag = 1)";
+
+                            // Update sorgusunu yürütme
+                            using (SqlCommand updateCommand = new SqlCommand(updateQuery, connection, transaction))
+                            {
+                                updateCommand.Parameters.Add(paramBeginDate);
+                                updateCommand.Parameters.Add(paramEndDate);
+                                updateCommand.ExecuteNonQuery();
+                            }
+
+                            // İşlemi gerçekleştirme
+                            transaction.Commit();
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        
+                        if (ex.Number == 1205) // Deadlock exception number
+                        {
+                            lock (this)
+                            {
+                                deadlockCountA++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void TypeBUserTransaction(string isolationLevel)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Transaction isolation level belirlemek için
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = $"SET TRANSACTION ISOLATION LEVEL {isolationLevel}";
+                    command.ExecuteNonQuery();
+                }
+
+                // Başlangıç ve bitiş tarihleri
+                DateTime beginDate = new DateTime(2011, 01, 01);
+                DateTime endDate = new DateTime(2015, 12, 31);
+
+                for (int i = 0; i < 100; i++)
+                {
+                    try
+                    {
+                        using (SqlTransaction transaction = connection.BeginTransaction())
+                        {
+                            // Rastgele tarih seçimi
+                            DateTime transactionDate = GetRandomDate(beginDate, endDate);
+
+                            // Select sorgusu için parametre değerleri
+                            SqlParameter paramBeginDate = new SqlParameter("@BeginDate", transactionDate);
+                            SqlParameter paramEndDate = new SqlParameter("@EndDate", transactionDate.AddYears(1).AddDays(-1));
+
+                            // Select sorgusu
+                            string selectQuery = "SELECT SUM(Sales.SalesOrderDetail.OrderQty) " +
+                                                 "FROM Sales.SalesOrderDetail " +
+                                                 "WHERE UnitPrice > 100 " +
+                                                 "AND EXISTS (SELECT * FROM Sales.SalesOrderHeader " +
+                                                             "WHERE Sales.SalesOrderHeader.SalesOrderID = Sales.SalesOrderDetail.SalesOrderID " +
+                                                             "AND Sales.SalesOrderHeader.OrderDate BETWEEN @BeginDate AND @EndDate " +
+                                                             "AND Sales.SalesOrderHeader.OnlineOrderFlag = 1)";
+                            
+                            var result =new SqlCommand( "SELECT * FROM Sales.SalesOrderDetail" , connection , transaction);
+                            using (var reader = result.ExecuteReader())
+                            {
+                                while (reader.Read())
+                                {
+                                    // Her bir satırı işleyin
+                                    // Örneğin:
+                                    var orderId = reader["SalesOrderID"].ToString();
+                                    var productId = reader["ProductID"].ToString();
+                                    // vb.
+                                }
+                            }
+                            // Select sorgusunu yürütme
+                            using (SqlCommand selectCommand = new SqlCommand(selectQuery, connection, transaction))
+                            {
+                                selectCommand.Parameters.Add(paramBeginDate);
+                                selectCommand.Parameters.Add(paramEndDate);
+                                selectCommand.ExecuteScalar();
+                            }
+
+                            // İşlemi gerçekleştirme
+                            transaction.Commit();
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        if (ex.Number == 1205) // Deadlock exception number
+                        {
+                            lock (this)
+                            {
+                                deadlockCountB++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private DateTime GetRandomDate(DateTime startDate, DateTime endDate)
+        {
+            Random rnd = new Random();
+            int range = (endDate - startDate).Days;
+            return startDate.AddDays(rnd.Next(range));
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            // Kullanıcı sayısını alın
+            int typeAUsers = int.Parse(textBox1.Text);
+            int typeBUsers = int.Parse(textBox2.Text);
+
+            // Seçilen izolasyon seviyesini alın
+            string selectedIsolationLevel = comboBox1.SelectedItem.ToString();
+
+            Random random = new Random();
+
+            List<Task> tasks = new List<Task>();
+             
+            // Type A ve Type B kullanıcı işlemlerini başlat
+            for (int i = 0; i < typeAUsers + typeBUsers; i++)
+            {
+                bool isTypeA = random.Next(100) < 50; // %50 olasılıkla Type A
+                string isolationLevel = isTypeA ? "READ UNCOMMITTED" : selectedIsolationLevel;
+
+                // Doğru türdeki işlemi başlat
+                if (isTypeA)
+                {
+                    tasks.Add(Task.Run(() => TypeAUserTransaction(isolationLevel)));
+                }
+                else
+                {
+                    tasks.Add(Task.Run(() => TypeBUserTransaction(isolationLevel)));
+                }
+            }
+
+            await Task.WhenAll(tasks);
+
+            // Deadlock sayısını göster
+            textBox3.Text = deadlockCountA.ToString();
+            textBox4.Text = deadlockCountB.ToString();
+
+            MessageBox.Show("Tüm işlemler tamamlandı.");
+        }
+
+        // Ön yüz ile ilgili diğer olaylar
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) { }
+
+        private void label1_Click(object sender, EventArgs e) { }
+
+        private void textBox2_TextChanged(object sender, EventArgs e) { }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+
+        private void label2_Click(object sender, EventArgs e) { }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
